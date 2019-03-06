aef1460dbe8fd0826b7bc3225c5ed05328108c5e
Unknown <e0008325@u.nus.edu>
2019-03-06 22:31:55 +0800

    add a bit of new layout components

--- 19 Telemetry App/app/src/main/java/com/example/fsae/a19telemetryapp/MainActivity.java
+++ 19 Telemetry App/app/src/main/java/com/example/fsae/a19telemetryapp/MainActivity.java
@@ -78,7 +78,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Speed = (TextView) findViewById(R.id.nav_engine);
+        Speed = (TextView) findViewById(R.id.speed);
+        Gear = (TextView) findViewById(R.id.gear);
+        RPM = (TextView) findViewById(R.id.rpm);
+
     }
 
     @Override
